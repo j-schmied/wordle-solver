114de9300c0020188d56ca2d9cccf64492290d22
Jannik Schmied <jannik.schmied@pm.me>
2023-08-13 18:29:30 +0200

    Slightly increased time between words, using builtin issubset function

--- spelling-bee-solver.py
+++ spelling-bee-solver.py
@@ -4,7 +4,7 @@ import time
 
 
 def load_words():
-    with open("en.txt") as word_file:
+    with open("./word_lists/en.txt") as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
@@ -16,14 +16,6 @@ def parse_args():
     parser.add_argument("--letters", "-l", type=str, required=True)
     parser.add_argument("--autosolve", action="store_true")
     return parser.parse_args()
-
-
-def set_match(word: str, letters: str) -> bool:
-    for char in word:
-        if char not in letters:
-            return False
-
-    return True
 
 
 def main():
@@ -40,11 +32,11 @@ def main():
         if len(word) >= 4 and definitive in word:
             distinct_letters = sorted(list(set(word.lower())))
 
-            if set_match(distinct_letters, letters):
+            if set(distinct_letters).issubset(set(letters)):
                 valid_word.append(word)
 
     print("-"*50)
-    print("SOLUTION")
+    print("SPELLING BEE SOLVER")
     print("-"*50)
 
     if args.autosolve:
@@ -57,7 +49,7 @@ def main():
         if args.autosolve:
             keyboard.write(word, delay=0.1)
             keyboard.press_and_release('enter')
-            time.sleep(0.5)
+            time.sleep(0.75)
 
         print(word)
 
